350e95b1301e222213d5e088abc6ced5041a8bdd
katyal5 <118711183+katyal5@users.noreply.github.com>
2022-12-17 11:28:33 +0300

    Add files via upload

---
+++ 5.pas
@@ -0,0 +1,24 @@
+﻿var inf:file of real; a,b,c,r,max:real; 
+begin 
+ assign(inf, 'TEXT.txt '); 
+ rewrite(inf); 
+ for var i := 1 to  10 do 
+ begin 
+ r:=random(1, 100); 
+ write(inf, r); 
+ end; 
+ close(inf); 
+ reset(inf); 
+ read(inf,a); 
+ read(inf,b); 
+ read(inf,c); 
+ print('Последний локальный максимум:'); 
+ while not eof(inf) do begin 
+ if b>a then if b>c then max:=b; 
+ a:=b; 
+ b:=c; 
+ read(inf,c); 
+ end; 
+ write(max); 
+ Close(inf); 
+end.
